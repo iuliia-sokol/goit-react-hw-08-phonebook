7bb78322c04eef42c39faba3d2fe6a3ecbda820c
Iuliia Sokol <avare779@gmail.com>
2023-01-12 22:22:01 +0200

    contacts route made restricted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -49,7 +49,7 @@ export const App = () => {
         <Route
           path="contacts"
           element={
-            <PrivateRoute redirectTo="/login">
+            <PrivateRoute restricted redirectTo="/login">
               <Contacts />
             </PrivateRoute>
           }
