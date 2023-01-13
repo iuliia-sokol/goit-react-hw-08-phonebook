37768055036a7034dfeecd76db3d73e5fd79c7ca
Iuliia Sokol <avare779@gmail.com>
2023-01-13 15:57:47 +0200

    delete modal responsive styles added

--- src/components/Modal/ModalDelete.styled.jsx
+++ src/components/Modal/ModalDelete.styled.jsx
@@ -21,36 +21,57 @@ export const AvatarWrapper = styled.div`
 `;
 
 export const Hint = styled.p`
-  font-size: 16px;
+  font-size: 14px;
   letter-spacing: 0.03em;
-  margin: 0;
+  margin: 0 0 16px 0;
+  text-align: center;
+
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+    margin: 0;
+  }
 `;
 
 export const ListItem = styled.div`
-  min-width: 50vw;
+  max-width: 65vw;
   display: flex;
   justify-content: center;
   align-items: center;
   font-weight: bold;
-  font-size: 20px;
-  padding: 32px;
+  font-size: 16px;
+  padding: 16px;
   border-radius: 12px;
   background-color: hsla(215, 98%, 79%, 0.2);
+
+  @media screen and (min-width: 768px) {
+    max-width: 50vw;
+    padding: 32px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 20px;
+  }
 `;
 
 export const ContentWrapper = styled.div`
   border-radius: 12px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 32px;
   padding: 32px;
   box-shadow: inset 2px 2px 5px #babecc, inset -5px -5px 10px #fff;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 
 export const ConfirmationText = styled.p`
   margin: 0;
   font-weight: normal;
+  text-align: center;
   & > span {
     font-weight: bold;
   }
