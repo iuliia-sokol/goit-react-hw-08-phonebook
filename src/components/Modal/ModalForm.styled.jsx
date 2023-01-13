ab1ae183b7b91d7d71846d8d4f9cadb67c43a36d
Iuliia Sokol <avare779@gmail.com>
2023-01-13 16:24:41 +0200

    update contact modal responsive styles added

--- src/components/Modal/ModalForm.styled.jsx
+++ src/components/Modal/ModalForm.styled.jsx
@@ -15,39 +15,63 @@ export const AvatarWrapper = styled.div`
   & > img {
     border-radius: 50%;
     width: 150px;
-    /* height: 120px; */
   }
 `;
+
 export const ListItem = styled.div`
-  min-width: 50vw;
+  max-width: 65vw;
   display: flex;
   justify-content: center;
   align-items: center;
   font-weight: bold;
-  font-size: 20px;
-  padding: 32px;
+  font-size: 16px;
+  padding: 16px;
   border-radius: 12px;
   background-color: hsla(215, 98%, 79%, 0.2);
+
+  @media screen and (min-width: 768px) {
+    max-width: 70vw;
+    padding: 32px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 20px;
+    max-width: fit-content;
+  }
 `;
 
 export const ContentWrapper = styled.div`
   border-radius: 12px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 32px;
   padding: 32px;
   box-shadow: inset 2px 2px 5px #babecc, inset -5px -5px 10px #fff;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
 `;
 
 export const ListItemForm = styled.form`
-  font-size: 20px;
+  font-size: 14px;
   color: #010101;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   gap: 16px;
   border-radius: 12px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    flex-direction: row;
+  }
 `;
 
 export const InputWrapper = styled.div`
@@ -55,17 +79,41 @@ export const InputWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 16px;
+  border-radius: 12px;
+  padding: 8px 12px;
+  box-shadow: inset 2px 2px 5px #babecc, inset -5px -5px 10px #fff;
 `;
 
-export const Label = styled.label``;
+export const Label = styled.label`
+  /* width: 100%; */
+  display: flex;
+  font-weight: bold;
+  font-size: 14px;
+  justify-content: space-between;
+  align-items: center;
+
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+  }
+`;
 
 export const Input = styled.input`
   min-width: 50%;
-  outline-color: yellow;
   border-radius: 8px;
-  font-size: 20px;
+  font-size: 12px;
   placeholder: grey;
-  padding: 12px 16px;
-  margin-left: 16px;
+  padding: 8px 12px;
+  outline-color: yellow;
   box-shadow: inset 2px 2px 5px #babecc, inset -5px -5px 10px #fff;
+  margin-left: 8px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 14px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    font-size: 16px;
+    margin-left: 0;
+    margin-left: 16px;
+  }
 `;
